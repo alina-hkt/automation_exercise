67d8737b4f54d3a39e898b231ea437aab7818d34
Alina <HKT.Alina@proton.me>
2026-08-20 12:55:34 +0300

    Fix login test: isolate data creation and wrap all steps in Allure

--- UI_TESTS/test_Login_User_correct.py
+++ UI_TESTS/test_Login_User_correct.py
@@ -1,10 +1,48 @@
 import pytest
+import time
 import allure
 from config import Config
 
 @pytest.mark.smoke
-def test_Login_User_Correct(home_page, login_page, account_deleted_page):
+def test_Login_User_Correct(home_page, register_page, login_page, account_deleted_page):
     config = Config()
+    username = "ALINA"
+    dynamic_email = f"alina_{int(time.time())}@test.com"
+        
+    home_page.open_home()
+    home_page.verify_logo_visible()
+    
+    home_page.click_login_signup()
+    
+    register_page.verify_new_user_signup_visible()
+    
+    register_page.fill_name(username)
+    register_page.fill_email(dynamic_email)
+    
+    register_page.click_signup()
+    
+    register_page.select_title_mrs()
+    register_page.fill_password(config.TEST_USER_PASSWORD)
+    register_page.set_date_of_birth(day="16", month="9", year="2004")
+    register_page.check_newsletter()
+            
+    register_page.fill_address_details(
+                first_name=username, last_name="TestUser", company="QA Corp",
+                address1="123 Test St", address2="Apt 1", country="United States",
+                state="California", city="Los Angeles", zipcode="90001", mobile="1234567890"
+            )
+    
+    register_page.click_create_account()
+    
+    register_page.verify_account_created()
+    
+    register_page.click_continue()
+    
+    home_page.verify_logged_in(username)
+
+    login_page.click_logout()
+            
+    login_page.verify_returned_to_login_page()
     
     with allure.step("Шаги 1-2: Открыть главную страницу и проверить загрузку"):
         home_page.open_home()
@@ -15,7 +53,7 @@ def test_Login_User_Correct(home_page, login_page, account_deleted_page):
         login_page.wait_for_login_form()
 
     with allure.step("Шаги 4-5: Ввести корректные учетные данные"):
-        login_page.fill_email(config.TEST_USER_EMAIL)
+        login_page.fill_email(dynamic_email)
         login_page.fill_password(config.TEST_USER_PASSWORD)
 
     with allure.step("Шаг 6: Нажать кнопку 'Login'"):
--- UI_TESTS/test_Logout_User.py
+++ UI_TESTS/test_Logout_User.py
@@ -1,10 +1,48 @@
 import pytest
+import time
 import allure
 from config import Config
 
 @pytest.mark.smoke
-def test_logout_user(home_page, login_page):
+def test_Login_User_Correct(home_page, register_page, login_page, account_deleted_page):
     config = Config()
+    username = "ALINA"
+    dynamic_email = f"alina_{int(time.time())}@test.com"
+        
+    home_page.open_home()
+    home_page.verify_logo_visible()
+    
+    home_page.click_login_signup()
+    
+    register_page.verify_new_user_signup_visible()
+    
+    register_page.fill_name(username)
+    register_page.fill_email(dynamic_email)
+    
+    register_page.click_signup()
+    
+    register_page.select_title_mrs()
+    register_page.fill_password(config.TEST_USER_PASSWORD)
+    register_page.set_date_of_birth(day="16", month="9", year="2004")
+    register_page.check_newsletter()
+            
+    register_page.fill_address_details(
+                first_name=username, last_name="TestUser", company="QA Corp",
+                address1="123 Test St", address2="Apt 1", country="United States",
+                state="California", city="Los Angeles", zipcode="90001", mobile="1234567890"
+            )
+    
+    register_page.click_create_account()
+    
+    register_page.verify_account_created()
+    
+    register_page.click_continue()
+    
+    home_page.verify_logged_in(username)
+
+    login_page.click_logout()
+            
+    login_page.verify_returned_to_login_page()
     
     with allure.step("1. Запустить браузер"):
         pass
@@ -22,14 +60,14 @@ def test_logout_user(home_page, login_page):
         login_page.wait_for_login_form()
         
     with allure.step("6. Ввести корректный email и пароль"):
-        login_page.fill_email(config.TEST_USER_EMAIL)
+        login_page.fill_email(dynamic_email)
         login_page.fill_password(config.TEST_USER_PASSWORD)
         
     with allure.step("7. Нажать кнопку 'login'"):
         login_page.click_login()
         
     with allure.step("8. Убедиться, что 'Logged in as username' виден"):
-        login_page.verify_logged_in()
+        login_page.verify_logged_in(username)
         
     with allure.step("9. Нажать кнопку 'Logout'"):
         login_page.click_logout()
