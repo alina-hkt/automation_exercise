0138fa4f8ff7e237976bc87a564f0620e357c91d
Alina <HKT.Alina@proton.me>
2026-08-21 17:06:42 +0300

    Fix_3 Test_6

--- UI_PAGES/contact_page.py
+++ UI_PAGES/contact_page.py
@@ -1,63 +1,56 @@
-import allure
 from playwright.sync_api import Page, expect
 from UI_PAGES.base_page import BasePage
-import os
 
 
 class ContactPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
-        
+
         self.name_input = page.get_by_placeholder("Name")
         self.email_input = page.get_by_placeholder("Email", exact=True)
         self.subject_input = page.get_by_placeholder("Subject")
         self.message_input = page.get_by_placeholder("Your Message Here")
-        self.upload_file_input = page.locator("input[name=\"upload_file\"]")
+        self.upload_file_input = page.locator("input[name='upload_file']")
         self.submit_btn = page.get_by_role("button", name="Submit")
+
+        self.success_message = page.locator(".status.alert-success").first
         
+        self.home_link = page.get_by_role("link", name="Home").first
+
         self.get_in_touch_heading = page.locator("h2").filter(has_text="GET IN TOUCH")
-        
-        self.home_link = page.get_by_role("link", name="Home") 
-        if not self.home_link.is_visible(timeout=self.config.PAGE_LOAD_TIMEOUT):
-             self.home_link = page.locator("img[alt='Website for automation practice']")
 
     def fill_contact_form(self, name: str, email: str, subject: str, message: str, file_path: str):
-        self.fill(self.name_input, name)
-        self.fill(self.email_input, email)
-        self.fill(self.subject_input, subject)
-        self.fill(self.message_input, message)
-        
+        expect(self.name_input).to_be_visible(timeout=self.config.PAGE_LOAD_TIMEOUT)
+        self.name_input.fill(name)
+
+        expect(self.email_input).to_be_visible(timeout=self.config.PAGE_LOAD_TIMEOUT)
+        self.email_input.fill(email)
+
+        expect(self.subject_input).to_be_visible(timeout=self.config.PAGE_LOAD_TIMEOUT)
+        self.subject_input.fill(subject)
+
+        expect(self.message_input).to_be_visible(timeout=self.config.PAGE_LOAD_TIMEOUT)
+        self.message_input.fill(message)
+
+        expect(self.upload_file_input).to_be_visible(timeout=self.config.PAGE_LOAD_TIMEOUT)
         self.upload_file_input.set_input_files(file_path)
-    
+        
+        self.page.wait_for_timeout(2000)
+
     def click_submit_and_handle_alert(self):
         self.page.on("dialog", lambda dialog: dialog.accept())
-        self.click(self.submit_btn)
-    
-    def verify_success_message(self):
-        locator = self.page.locator("#contact-page .alert-success").filter(
-            has_text="Success! Your details have been submitted successfully."
-        )
         
-        try:
-            expect(locator).to_be_visible(timeout=self.config.PAGE_LOAD_TIMEOUT)
-        except AssertionError:
-            if not os.getenv("CI"):
-                screenshot_path = "debug_contact_success.png"
-                self.page.screenshot(path=screenshot_path)
-                allure.attach.file(
-                    screenshot_path, 
-                    name="Debug Screenshot", 
-                    attachment_type=allure.attachment_type.PNG
-                )
-                
-                html_content = self.page.content()
-                allure.attach(
-                    html_content, 
-                    name="Page HTML on Failure", 
-                    attachment_type=allure.attachment_type.TEXT
-                )
-            
-            raise AssertionError("Сообщение об успехе не найдено.")
+        self.click(self.submit_btn)
+        
+        self.page.wait_for_load_state("networkidle", timeout=self.config.PAGE_LOAD_TIMEOUT)
+
+    def verify_success_message(self):
+        expect(self.success_message).to_have_count(1, timeout=self.config.PAGE_LOAD_TIMEOUT)
+        
+        expect(self.success_message).to_contain_text(
+            "Success! Your details have been submitted successfully.", 
+            timeout=self.config.PAGE_LOAD_TIMEOUT
+        )
 
     def click_home(self):
         self.click(self.home_link)
